926d00ee5fae11d9e9892144f45a306f45c44ff4
RejmanDavid <rejmada1@uhk.cz>
2023-03-08 22:33:13 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Jeremy";
+        int age = 22;
+        String cls = "PRO3";
+
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            Connection con = DriverManager.getConnection("jdbc:derby:exampleDB;create=true");
+
+            Statement st = con.createStatement();
+
+            //st.addBatch("drop table student");
+            //st.addBatch("delete from student");
+            st.addBatch("create table student (id int not null generated always as " +
+                    "identity constraint student_PK primary key, " +
+                    "name varchar(20),age int,class varchar(5))");
+            st.executeBatch();
+
+            st = con.createStatement();
+            st.addBatch("insert into student (name,age,class) values" +
+                    "('" + name + "'," + age + ",'" + cls + "')");
+            st.executeBatch();
+
+            ResultSet res = st.executeQuery("select * from student");
+            while(res.next()){
+                System.out.print(res.getString("id")+" ");
+                System.out.print(res.getString("name")+" ");
+                System.out.print(res.getString("age")+" ");
+                System.out.println(res.getString("class")+" ");
+            }
+
+            con.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
